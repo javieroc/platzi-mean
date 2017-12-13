27ac76d6c48f4a5bcd995a60693830e24eb0bf5d
Javier Ocampo <ferroxido@gmail.com>
2017-12-13 15:04:05 -0300

    Add signin and signup forms

--- src/app/answer/answer-form.component.ts
+++ src/app/answer/answer-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Answer, User } from './answer.model';
+import { Answer } from './answer.model';
+import { User } from '../auth/user.model';
 import { Question } from '../question/question.model';
 
 @Component({
@@ -15,7 +16,7 @@ export class AnswerFormComponent {
       form.value.description,
       this.question,
       new Date(),
-      new User('Luciana', 'Orellana')
+      new User(null, null, 'Luciana', 'Orellana')
     );
     this.question.answers.unshift(answer);
     form.reset();
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 // Material angular
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -14,20 +14,25 @@ import { MomentModule } from 'angular2-moment';
 import { AppComponent } from './app.component';
 import { QuestionDetailComponent } from './question/question-detail.component';
 import { AnswerFormComponent } from './answer/answer-form.component';
+import { SigninScreenComponent } from './auth/signin-screen.component';
+import { SignupScreenComponent } from './auth/signup-screen.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     QuestionDetailComponent,
-    AnswerFormComponent
+    AnswerFormComponent,
+    SigninScreenComponent,
+    SignupScreenComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
     MomentModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
