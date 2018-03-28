29f29732c8bc4827ff99418889c74028633b9689
Javier Ocampo <ferroxido@gmail.com>
2018-03-28 12:39:43 -0300

    Add models

---
+++ server/models/answer.js
@@ -0,0 +1,15 @@
+import mongoose, { Schema } from 'mongoose';
+
+const AnswerSchema = new Schema({
+  description: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+});
+
+export default mongoose.model('Answer', AnswerSchema);
